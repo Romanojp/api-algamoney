f63d56cdeb6870f36ff7df1b4f68b1dd641f2ecb
Romanojp <jp_romana@outlook.com>
2022-07-26 15:51:56 -0300

    FAS_7.5-nome-usuario-no-token-jwt-v1

--- src/main/java/com/example/algamoney/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/example/algamoney/api/config/AuthorizationServerConfig.java
@@ -1,55 +1,69 @@
 package com.example.algamoney.api.config;
 
 
+
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
-@SuppressWarnings("deprecation")
+import com.example.algamoney.api.config.token.CustomTokenEnhancer;
+
+@Profile("oauth-security")
 @Configuration
+@SuppressWarnings("deprecation")
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
-    @Autowired
-	private AuthenticationManager authenticationManager;
 
 	@Autowired
-	private PasswordEncoder passwordEncoder;
-    	
+	private AuthenticationManager authenticationManager;
+	
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory()
 				.withClient("angular")
-				.secret(passwordEncoder.encode("@ngul@r0")) // @ngul@r0
+				.secret("@ngul@r0")
 				.scopes("read", "write")
+				.authorizedGrantTypes("password", "refresh_token")
+				.accessTokenValiditySeconds(1800)
+				.refreshTokenValiditySeconds(3600 * 24)
+			.and()
+				.withClient("mobile")
+				.secret("m0b1l30")
+				.scopes("read")
 				.authorizedGrantTypes("password", "refresh_token")
 				.accessTokenValiditySeconds(1800)
 				.refreshTokenValiditySeconds(3600 * 24);
 	}
-
+	
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+		tokenEnhancerChain.setTokenEnhancers(Arrays.asList(tokenEnhancer(), accessTokenConverter()));
+		
 		endpoints
-			.authenticationManager(authenticationManager)
-			.accessTokenConverter(accessTokenConverter())
 			.tokenStore(tokenStore())
-			.reuseRefreshTokens(false);
+			.tokenEnhancer(tokenEnhancerChain)
+			.reuseRefreshTokens(false)
+			.authenticationManager(authenticationManager);
 	}
-
+	
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
-
-		accessTokenConverter.setSigningKey("3032885ba9cd6621bcc4e7d6b6c35c2b");
-
+		accessTokenConverter.setSigningKey("algaworks");
 		return accessTokenConverter;
 	}
 
@@ -57,6 +71,10 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	public TokenStore tokenStore() {
 		return new JwtTokenStore(accessTokenConverter());
 	}
-
+	
+	@Bean
+	public TokenEnhancer tokenEnhancer() {
+	    return (TokenEnhancer) new CustomTokenEnhancer();
+	}
 
 }
